e738c900f2bee22939a00a7921412781bc3b1b20
Tommy <tmarriaga@gmail.com>
2018-02-26 20:23:54 -0500

    Funcionalidad sumar terminada sin validaciones

---
+++ app/src/main/java/com/holamundo/operaciones/Metodos.java
@@ -0,0 +1,14 @@
+package com.holamundo.operaciones;
+
+/**
+ * Created by android on 26/02/2018.
+ */
+
+public class Metodos {
+
+    public static double sumar(double n1, double n2){
+        return n1+n2;
+    }
+
+
+}
--- app/src/main/java/com/holamundo/operaciones/Principal.java
+++ app/src/main/java/com/holamundo/operaciones/Principal.java
@@ -2,12 +2,33 @@ package com.holamundo.operaciones;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+
+    private EditText n1,n2;
+    private TextView res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        n1=findViewById(R.id.txtNumero1);
+        n2=findViewById(R.id.txtNumero2);
+        res=findViewById(R.id.lblResultado);
     }
+
+    public void calcular(View v){
+        double num1, num2,resultado;
+        num1= Double.parseDouble(n1.getText().toString());
+        num2= Double.parseDouble(n2.getText().toString());
+
+        resultado=Metodos.sumar(num1,num2);
+        res.setText(""+resultado);
+
+    }
+
 }
