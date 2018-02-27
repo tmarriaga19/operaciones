3d0e865f6c6589eb289550d968a23e0f6a2e4e1b
Tommy <tmarriaga@gmail.com>
2018-02-26 20:58:50 -0500

    Aplicacion bilingue suma terminada

--- app/src/main/java/com/holamundo/operaciones/Principal.java
+++ app/src/main/java/com/holamundo/operaciones/Principal.java
@@ -1,5 +1,6 @@
 package com.holamundo.operaciones;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ public class Principal extends AppCompatActivity {
 
     private EditText n1,n2;
     private TextView res;
+    private Resources recursos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +21,31 @@ public class Principal extends AppCompatActivity {
         n1=findViewById(R.id.txtNumero1);
         n2=findViewById(R.id.txtNumero2);
         res=findViewById(R.id.lblResultado);
+        recursos=this.getResources();
     }
 
     public void calcular(View v){
         double num1, num2,resultado;
-        num1= Double.parseDouble(n1.getText().toString());
-        num2= Double.parseDouble(n2.getText().toString());
 
-        resultado=Metodos.sumar(num1,num2);
-        res.setText(""+resultado);
+        if (n1.getText().toString().isEmpty()){
+            n1.requestFocus();
+            n1.setError(recursos.getString(R.string.error_numero1));
+        }else if(n2.getText().toString().isEmpty()){
+            n2.requestFocus();
+            n2.setError(recursos.getString(R.string.error_numero2));
+        } else{
+            num1= Double.parseDouble(n1.getText().toString());
+            num2= Double.parseDouble(n2.getText().toString());
+            resultado=Metodos.sumar(num1,num2);
+            res.setText(""+resultado);
+        }
+    }
 
+    public void limpiar (View v){
+        res.setText("");
+        n1.setText("");
+        n2.setText("");
+        n1.requestFocus();
     }
 
 }
