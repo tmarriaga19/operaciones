111c928b9d35b8c5fed4e93150b16a5230d74415
Tommy <tmarriaga@gmail.com>
2018-03-05 21:24:23 -0500

    Operaciones con testing

--- app/src/test/java/com/holamundo/operaciones/ExampleUnitTest.java
+++ app/src/test/java/com/holamundo/operaciones/ExampleUnitTest.java
@@ -10,8 +10,32 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void sumar_dos_numeros_correctamente(){
+        double  a = 4, b=5 ;
+        double res = Metodos.sumar(a,b);
+        assertEquals(9,res,0 );
+    }
+
+    @Test
+    public void restar_dos_numeros_correctamente(){
+        double  a = 4, b=5 ;
+        double res = Metodos.restar(a,b);
+        assertEquals(-1,res,0 );
+    }
+
+    @Test
+    public void multiplicar_dos_numeros_correctamente(){
+        double  a = 4, b=5 ;
+        double res = Metodos.multiplicar(a,b);
+        assertEquals(20,res,0 );
+    }
+
+    @Test
+    public void dividir_dos_numeros_correctamente(){
+        double  a = 4, b=2 ;
+        double res = Metodos.dividir(a,b);
+        assertEquals(2,res,0 );
     }
 }
